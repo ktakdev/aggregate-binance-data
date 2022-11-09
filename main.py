90ea22f5cbfbebeb56b9b67f17f52884c14c9e73
ktak <ktak.dev@gmail.com>
2022-11-10 06:54:05 +0900

    add black/isort /flake8 setting

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from datetime import datetime, timedelta
-from google.cloud import bigquery
+
 from binance import Client
+from google.cloud import bigquery
+
 
 class Config:
     binance_api_key = os.environ.get("BINANCE_API_KEY")
@@ -9,18 +11,27 @@ class Config:
     watch_list_table = os.environ.get("BIGQUERY_WATCH_LIST_TABLE")
     output_table = os.environ.get("BIGQUERY_OUTPUT_TABLE")
 
+
 bq = bigquery.Client()
 binance = Client(api_key=Config.binance_api_key, api_secret=Config.binance_api_secret)
 
+
 def execute(request, context):
-    start_timestamp = datetime.timestamp(datetime.now().replace(minute=0, second=0) - timedelta(hours = 1))
+    start_timestamp = datetime.timestamp(
+        datetime.now().replace(minute=0, second=0) - timedelta(hours=1)
+    )
 
     watch_list_table = bq.get_table(table=Config.watch_list_table)
     watch_list = bq.list_rows(watch_list_table)
     result_rows = []
     for watch_list_item in watch_list:
         (base_asset, quote_asset, derivative_availale) = watch_list_item.values()
-        row = aggregate_data(base_asset, quote_asset, start_timestamp, fetch_derivative_data=derivative_availale)
+        row = aggregate_data(
+            base_asset,
+            quote_asset,
+            start_timestamp,
+            fetch_derivative_data=derivative_availale,
+        )
 
         result_rows.append(row)
         print(row)
@@ -32,9 +43,14 @@ def execute(request, context):
 
     return result_rows
 
-def aggregate_data(base_asset, quote_asset, start_timestamp, fetch_derivative_data = True):
+
+def aggregate_data(
+    base_asset, quote_asset, start_timestamp, fetch_derivative_data=True
+):
     symbol = base_asset + quote_asset
-    klines = binance.get_historical_klines(symbol=symbol, interval="1h", start_str=str(start_timestamp), limit=1)
+    klines = binance.get_historical_klines(
+        symbol=symbol, interval="1h", start_str=str(start_timestamp), limit=1
+    )
     [
         open_time,
         open_price,
@@ -47,11 +63,11 @@ def aggregate_data(base_asset, quote_asset, start_timestamp, fetch_derivative_da
         number_of_trade,
         _,
         taker_buy_quote_asset_volume,
-        _
+        _,
     ] = klines[0]
     row = {}
-    row["base_asset"]  = base_asset
-    row["quote_asset"]  = quote_asset
+    row["base_asset"] = base_asset
+    row["quote_asset"] = quote_asset
     row["open_time"] = open_time / 1000
     row["close_time"] = close_time / 1000
     row["open_price"] = float(open_price)
@@ -65,14 +81,22 @@ def aggregate_data(base_asset, quote_asset, start_timestamp, fetch_derivative_da
         row["taker_buy_volume"] = float(taker_buy_quote_asset_volume)
     # convert to USDT volume
     else:
-        quote_asset_usdt_price = binance.get_ticker(symbol=quote_asset + "USDT")["lastPrice"]
+        quote_asset_usdt_price = binance.get_ticker(symbol=quote_asset + "USDT")[
+            "lastPrice"
+        ]
         row["volume"] = float(quote_asset_volume) * float(quote_asset_usdt_price)
-        row["taker_buy_volume"] = float(taker_buy_quote_asset_volume) * float(quote_asset_usdt_price)
+        row["taker_buy_volume"] = float(taker_buy_quote_asset_volume) * float(
+            quote_asset_usdt_price
+        )
 
     # only for derivative
     if fetch_derivative_data:
-        oi = binance.futures_open_interest_hist(symbol=symbol, period="1h", start_str=str(start_timestamp), limit=1)
-        ratio = binance.futures_top_longshort_position_ratio(symbol=symbol, period="1h", limit=1)
+        oi = binance.futures_open_interest_hist(
+            symbol=symbol, period="1h", start_str=str(start_timestamp), limit=1
+        )
+        ratio = binance.futures_top_longshort_position_ratio(
+            symbol=symbol, period="1h", limit=1
+        )
         row["open_interest"] = float(oi[0]["sumOpenInterestValue"])
         row["longshort_position_ratio"] = float(ratio[0]["longShortRatio"])
     else:
